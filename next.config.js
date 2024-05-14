327f54eea509f309af85160f922a4ef0bbf09bbe
hrithikscube <hrithik@scube.me>
2024-05-14 16:41:13 +0530

    image sequence prototype animation

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig)
