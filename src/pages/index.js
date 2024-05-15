cfbb8345bf94d954237bcfe6f7c74db13c2ecffe
hrithikscube <hrithik@scube.me>
2024-05-15 16:41:49 +0530

    updated homepage sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,8 +61,10 @@ const Home = () => {
   return (
     <div className='relative bg-black text-white'>
 
-      <div className='flex flex-col items-center justify-center text-white h-screen  relative'>
-        <div className='w-full h-screen flex items-center justify-center pin-this z-[19] lg:p-0 px-4'>
+      {/* hero animation */}
+
+      <div className='flex flex-col items-center justify-center text-white lg:h-screen min-h-screen  relative'>
+        <div className='w-full h-screen flex items-center justify-center pin-this z-[19]'>
           <img
             id="sequence-img"
             ref={imageRef}
@@ -76,32 +78,99 @@ const Home = () => {
         <div className='w-full h-screen flex-shrink-0 flex flex-col items-center justify-start pt-44 absolute z-[18]'>
 
           <div className='flex flex-col items-center justify-center text-center -mt-1'>
-            <h1 className=' lg:text-xl text-center'>Smarter Sips, Effortless Wellbeing.</h1>
-            <h2 className='text-cyanLight opacity-90 lg:text-8xl font-semibold'>Series L1</h2>
+            <h1 className='lg:text-xl text-base text-center'>Smarter Sips, Effortless Wellbeing.</h1>
+            <h2 className='text-cyanLight opacity-90 lg:text-8xl text-5xl font-semibold'>Series L1</h2>
           </div>
 
+
+
+        </div>
+
+        <div className='w-full h-full flex items-end justify-center pb-20 absolute z-[22]'>
+          <button className='h-[42px] px-7 rounded-full bg-blue-600 hover:bg-blue-500 text-white text-sm font-medium'>
+            Order Now
+          </button>
         </div>
       </div>
 
       <div className='z-[20] relative'>
 
         <div className='w-full h-screen flex-shrink-0 flex flex-col items-center justify-center'>
-          <div className='flex flex-col items-center justify-center text-center gap-2'>
-            <h1 className='text-white lg:text-xl text-center'>LWL8 - Smart Water Bottle Is Your Ultimate Companion!</h1>
-            <h2 className='text-cyanLight opacity-90 lg:text-6xl font-semibold'>Ultimate Campanion!</h2>
+          <div className='flex flex-col items-center justify-center text-center lg:gap-2'>
+            <h1 className='text-white lg:text-xl text-base text-center lg:w-full w-9/12 mx-auto'>LWL8 - Smart Water Bottle Is Your Ultimate Companion!</h1>
+            <h2 className='text-cyanLight opacity-90 lg:text-6xl text-3xl font-semibold'>Ultimate Campanion!</h2>
           </div>
         </div>
 
         <div className='w-full h-screen flex-shrink-0 flex flex-col items-center justify-center'>
           <div className='flex flex-col items-center justify-center text-center gap-2'>
-            <h2 className='text-white lg:text-6xl font-semibold'>Let's Discover Why</h2>
+            <h2 className='text-white lg:text-6xl text-3xl font-semibold'>Let's Discover Why</h2>
           </div>
         </div>
       </div>
 
 
-      <div className='bg-purple-500 w-full h-screen flex items-center justify-center'>
-        Hello world
+      {/* remaining sections */}
+
+      <div className='bg-black w-full lg:h-screen min-h-screen flex lg:flex-row flex-col items-center justify-center'>
+
+        <div className='lg:w-6/12 h-full flex-shrink-0 w-full lg:p-0 p-6'>
+          <div className='flex flex-col items-start justify-center text-start lg:gap-2 w-full h-full lg:pl-20 lg:w-11/12'>
+            <h1 className='text-white lg:text-xl text-sm text-start'>Premium Safety Assurance</h1>
+            <h2 className='text-cyanLight opacity-90 lg:text-4xl text-xl font-semibold'>Superior Safety with
+            </h2>
+            <h2 className='text-white opacity-90 lg:text-4xl text-xl font-semibold'>
+              Tritan Food-Grade Material
+            </h2>
+          </div>
+        </div>
+
+        <div className='lg:w-6/12 h-full flex-shrink-0 lg:p-10 p-6'>
+          <img src="section-1-bottle.webp" alt="section" className='w-full h-full object-contain' />
+        </div>
+
+      </div>
+
+      <div className='bg-black w-full lg:h-screen min-h-screen flex lg:flex-row-reverse flex-col-reverse items-center justify-center'>
+
+        <div className='lg:w-6/12 h-full flex-shrink-0 lg:p-0 p-6 w-full'>
+          <div className='flex flex-col items-start justify-center text-start lg:gap-2 w-full h-full lg:pl-20 lg:w-11/12'>
+            <h1 className='text-white lg:text-xl text-sm text-start'>Ergonomic comfort</h1>
+            <h2 className='text-cyanLight opacity-90 lg:text-4xl text-xl font-semibold'>Know how much water you drink
+            </h2>
+            <h2 className='text-white opacity-90 lg:text-4xl text-xl font-semibold'>
+              and get reminders in case you forget
+            </h2>
+          </div>
+        </div>
+
+        <div className='lg:w-6/12 h-full flex-shrink-0 lg:p-10 p-6'>
+          <img src="section-2-bottle.webp" alt="section" className='w-full h-full object-contain' />
+        </div>
+
+
+
+      </div>
+
+      <div className='bg-black w-full lg:h-screen min-h-screen lg:flex hidden lg:flex-row flex-col items-center justify-center'>
+
+        <div className='w-full h-full absolute '>
+          <div className='lg:w-6/12 h-full flex-shrink-0 w-full lg:p-0 p-6'>
+            <div className='flex flex-col items-start justify-center text-start lg:gap-2 w-full h-full lg:pl-20 '>
+              <h1 className='text-white lg:text-xl text-sm text-start'>Hydration Monitoring and Reminders</h1>
+              <h2 className='text-cyanLight opacity-90 lg:text-4xl text-xl font-semibold'>Unmatched Comfort and
+              </h2>
+              <h2 className='text-white opacity-90 lg:text-4xl text-xl font-semibold'>
+                <span className='text-cyanLight'>Adaptability</span> with Ergonomic Design
+              </h2>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full h-full flex-shrink-0'>
+          <img src="section-3-bottle.webp" alt="section" className='w-full h-full object-contain' />
+        </div>
+
       </div>
 
 
